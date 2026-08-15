73d0698b76886bcf47b5ab7f59ff0ea708aa66f3
Stefan <stefan@openclaw>
2026-08-15 15:17:46 +0200

    chore: register GatedDeltaNet in umbrella, Makefile, EXPANSION_QUEUE

--- include/nn/nn.h
+++ include/nn/nn.h
@@ -99,6 +99,7 @@
 #include "layers/recurrent/mlstm.h"
 #include "layers/recurrent/h3.h"
 #include "layers/recurrent/deltanet.h"
+#include "layers/recurrent/gated_deltanet.h"
 #include "layers/recurrent/gla.h"
 #include "layers/recurrent/hawk.h"
 #include "layers/recurrent/hgrn.h"
